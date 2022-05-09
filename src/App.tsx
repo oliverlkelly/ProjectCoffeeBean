a5f12d4f312c4bfe2b5c05fc6b2ff6ae2938bb6d
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-05-09 22:39:00 +1000

    COFB-6 Fixed error
    getting uncaught error due to not importing BrowserRouter correctly.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
 import logo from './logo.svg';
+import Navigation from "./components/navigation/navigation";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation/>
+      <Routes/>
+    </Router>
   );
 }
 
